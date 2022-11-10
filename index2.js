d7bc0e56e6dd9a7b226efe5eccb233e5538394f1
somtochukwu nwosu <somynwosu@gmail.com>
2022-11-10 09:12:19 +0100

    added a loader

--- index2.js
+++ index2.js
@@ -21,3 +21,13 @@ function generateUser(){
     console.log(data)})
 }
 
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
